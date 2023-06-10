8c48c73389659b3bec53bc2fa7278b1c33905ccc
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-11 02:55:26 +0700

    define function addtoCart, removeFromCart

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
 import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element= {<Shop />}/>
-        <Route path="/contact" />
-        <Route path="/cart" element= {<Cart />}/>
-      </Routes>
-    </Router>
+    <ShopContextProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element= {<Shop />}/>
+          <Route path="/contact" />
+          <Route path="/cart" element= {<Cart />}/>
+        </Routes>
+      </Router>
+    </ShopContextProvider>
   );
 }
 
--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,10 +1,35 @@
 import React, { createContext, useState } from 'react';
+import { PRODUCTS } from '../products'; 
 
 export const ShopContext = createContext(null);
 
+// handle the change of adding product to card
+// because the id start with 1 so we need to +1 in product leght
+const getDefaultCart = () => {
+    let cart = {};
+    for (let i = 1; i < PRODUCTS.length +1; i++) {
+        cart[i] = 0
+    }
+    return cart;
+}
+
 export const ShopContextProvider = (props) => {
-    const [cartItem, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    // add item to cart giu nguyen gia tri cua id va cong them 1
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
+    }
+
+    // remove from cart
+    const removeFromcart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
+    }
+
+    const contextValue ={cartItems, addToCart, removeFromcart}
     return (
-        <ShopContext.Provider>{props.children}</ShopContext.Provider>
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
     )
 }
