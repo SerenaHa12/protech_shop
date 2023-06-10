4e6450cc60c9c2d45c23af8eff4d6345e0e624bc
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-11 05:12:54 +0700

    nhieu bug qua huhuhu

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
+// import { Contact } from "./pages/contact";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
-    <ShopContextProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element= {<Shop />}/>
-          <Route path="/contact" />
-          <Route path="/cart" element= {<Cart />}/>
-        </Routes>
-      </Router>
-    </ShopContextProvider>
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
   );
 }
 
