cc2ca2389bf498f16ec28b679e58c9f71f097833
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-12 01:31:19 +0700

    update full function cart

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,36 +1,60 @@
-import React, { createContext, useState } from 'react';
-import { PRODUCTS } from '../products'; 
+import { createContext, useEffect, useState } from "react";
+import { PRODUCTS } from "../products";
 
 export const ShopContext = createContext(null);
 
 // handle the change of adding product to card
-// because the id start with 1 so we need to +1 in product leght
+// because the id start with 1 so we need to +1 in product length
 const getDefaultCart = () => {
-    let cart = {};
-    for (let i = 1; i < PRODUCTS.length +1; i++) {
-        cart[i] = 0
-    }
-    return cart;
-}
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
 
 export const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState(getDefaultCart());
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    // add item to cart giu nguyen gia tri cua id va cong them 1
-    const addToCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
     }
+    return totalAmount;
+  };
 
-    // remove from cart
-    const removeFromcart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
-    }
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
 
-    const contextValue ={cartItems, addToCart, removeFromcart};
-    console.log(cartItems);
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
+
+  const checkout = () => {
+    setCartItems(getDefaultCart());
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    updateCartItemCount,
+    removeFromCart,
+    getTotalCartAmount,
+    checkout,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
